ebf9501bcf828e51fe73591dcbbd94bee273e25c
fu1ny2t <180347055+fu1ny2t@users.noreply.github.com>
2025-01-31 22:57:33 -0500

    Update blip_buf.cpp

--- Utilities/blip_buf.cpp
+++ Utilities/blip_buf.cpp
@@ -214,15 +214,7 @@ static void remove_samples( blip_t* m, int count )
 {
 	buf_t* buf = m->buffer;
 
-	int lpf_taps;
-	switch( blip_sample_rate ) {
-	case 768000: lpf_taps = lpf_step_768_taps; break;
-	case 384000: lpf_taps = lpf_step_384_taps; break;
-	case 192000: lpf_taps = lpf_step_192_48_taps; break;
-	case 96000: lpf_taps = lpf_step_96_48_taps; break;
-	case 48000: lpf_taps = lpf_step_48_taps; break;
-	default: lpf_taps = 0; break;
-	}
+	int lpf_taps = blip_lpf_taps(blip_sample_rate);
 
 	int remain = (m->offset >> time_bits) - count;
 	if( lpf_taps > remain ) remain = lpf_taps;
@@ -302,44 +294,7 @@ void blip_add_delta( blip_t* m, unsigned time, int delta )
 	assert( pos <= m->size );
 #endif
 
-	/* 31-bit * 15-bit = 46-bit >> 15 = 31-bit */
-	switch( blip_sample_rate ) {
-#if 1
-	case 768000:
-		for( int lcv = 0; lcv < lpf_step_768_taps; lcv++ ) {
-			out [lcv] += ((signed long long)lpf_step_768[lcv] * delta) / lpf_scale;
-		}
-		break;
-
-	case 384000:
-		for( int lcv = 0; lcv < lpf_step_384_taps; lcv++ ) {
-			out [lcv] += ((signed long long)lpf_step_384[lcv] * delta) / lpf_scale;
-		}
-		break;
-
-	case 192000:
-		for( int lcv = 0; lcv < lpf_step_192_48_taps; lcv++ ) {
-			out [lcv] += ((signed long long)lpf_step_192_48[lcv] * delta) / lpf_scale;
-		}
-		break;
-
-	case 96000:
-		for( int lcv = 0; lcv < lpf_step_96_48_taps; lcv++ ) {
-			out [lcv] += ((signed long long)lpf_step_96_48[lcv] * delta) / lpf_scale;
-		}
-		break;
-
-	case 48000:
-		for( int lcv = 0; lcv < lpf_step_48_taps; lcv++ ) {
-			out [lcv] += ((signed long long)lpf_step_48[lcv] * delta) / lpf_scale;
-		}
-		break;
-#endif
-
-	default:
-		out [0] += (buf_t) delta * (1UL << lpf_frac);
-		break;
-	}
+	blip_lpf_mono(blip_sample_rate, out, delta);
 }
 
 void blip_add_delta_fast( blip_t* m, unsigned time, int delta )
